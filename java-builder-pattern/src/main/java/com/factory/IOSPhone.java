7205bcd93b7de4236430c1ec15d40b1a9db5b0b2
bhavuk sharma <bhavuk55555@gmail.com>
2025-12-04 23:35:47 +0530

    factory pattern

---
+++ java-builder-pattern/src/main/java/com/factory/IOSPhone.java
@@ -0,0 +1,9 @@
+package com.factory;
+
+public class IOSPhone implements IMobilePhone{
+
+    @Override
+    public void show(){
+        System.out.println("I am an Iphone, most expensive and few lags in the software");
+    }
+}
